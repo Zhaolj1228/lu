c265303d6e12125296bb53707296c9e9c5372885
SC-201901291116\lanou <996800597@qq.com>
2019-05-06 20:50:33 +0800

    no message

--- vueitem/src/App.vue
+++ vueitem/src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <Header></Header>
+    <div>
+      <Header></Header>
+      <router-link :to="{path:'/child1'}">点我</router-link>
+    </div>
+    <div>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
   import Header from './components/Header'
-export default {
-  name: 'App',
-  components:{
-    Header
+
+  export default {
+    name: 'App',
+    components: {
+      Header
+    }
   }
-}
 </script>
 
 <style>
