de8df3c1c35db5acaa3e9052cd97e1336a60d19e
DESKTOP-5TBM399\lx <346880761@qq.com>
2019-05-06 21:00:58 +0800

    child2

--- vueitem/src/router/index.js
+++ vueitem/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Child1 from '../components/Child1'
+import Child2 from '../components/Child2'
 Vue.use(Router)
 
 export default new Router({
@@ -8,6 +9,10 @@ export default new Router({
     {
       path:'/child1',
       component:Child1
+    },
+    {
+      path:'/child2',
+      component:Child2
     }
   ]
 })
