e289e19ab5276a30ec7174e9b9c004df542af006
SC-201901291116\lanou <996800597@qq.com>
2019-05-06 20:53:36 +0800

    no message

--- vueitem/src/router/index.js
+++ vueitem/src/router/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Child1 from '../components/Child1'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-
+    {
+      path:'/child1',
+      component:Child1
+    }
   ]
 })
